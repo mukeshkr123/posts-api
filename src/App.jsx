dd7ede1d142355c3452f3721c2e63faf84bba67c
mukesh <mukeshmehta2041@gmail.com>
2023-07-15 09:04:57 +0530

    adding form and sending data to the addpost

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import apiClient from "./api-client/apiClient";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+//post schema
+const postSchema = z.object({
+  id: z.string().min(2, "Minimum 2 digits are required"),
+  title: z.string().min(2, "Minimum 2 digits are required"),
+});
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [isloading, setIsloading] = useState(false);
   const [error, setError] = useState("");
+
+  const { register, handleSubmit, reset } = useForm();
 
   useState(() => {
     const fetchUser = async () => {
@@ -23,7 +34,6 @@ function App() {
   }, []);
 
   //delete the posts
-
   const onDelete = (id) => {
     const originalPost = [...posts];
     setPosts(posts.filter((post) => post.id !== id));
@@ -34,12 +44,12 @@ function App() {
   };
 
   //add posts
-  const addPost = () => {
+  const addPost = (data) => {
+    console.log(data);
     const originalPosts = [...posts];
-    const addnewPost = { id: 0, title: "new POST" };
-    setPosts([addnewPost, ...posts]);
+    setPosts([data, ...posts]);
     apiClient
-      .post("/posts" + addnewPost)
+      .post("/posts" + data)
       .then(({ data: savedPosts }) => setPosts([savedPosts, ...posts]))
       .catch((err) => {
         setError(err.message);
@@ -61,15 +71,43 @@ function App() {
 
   return (
     <>
-      <h1>Post api </h1>
-      {error && <p className="text-danger"> {error} </p>}
-      <button onClick={addPost} className=" btn btn-primary">
-        ADD
-      </button>
+      <h1 className="mx-4">Post api </h1>
+      {error && <p className=" mx-4 text-danger"> {error} </p>}
+      <form
+        onSubmit={handleSubmit((data) => {
+          reset();
+          addPost(data);
+        })}
+        className="form-group m-3"
+      >
+        <label className="my-1" htmlFor="">
+          #id
+        </label>
+        <input
+          {...register("id")}
+          type="text"
+          id="id"
+          name="id"
+          className="my-1 form-control"
+          placeholder="Enter Post Id"
+        />
+        <label className="my-1" htmlFor="">
+          Tittle
+        </label>
+        <input
+          type="text"
+          {...register("title")}
+          id="title"
+          name="title"
+          className="my-1 form-control"
+          placeholder="Enter Title"
+        />
+        <input type="submit" className="s m-3 btn btn-primary" value="Submit" />
+      </form>
       {isloading ? (
         <div className="spinner-border"></div>
       ) : (
-        <ul className="list-group ">
+        <ul className="list-group m-3 ">
           {posts.map((post) => (
             <li
               key={post.id}
